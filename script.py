4c8467e7f5bdc4b582332c9347c9b6856c293b5b
Nikita Dubovik <136176084+iamochenmili@users.noreply.github.com>
2024-08-11 17:16:36 +0300

    файлы для статы

---
+++ script.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+# Данные о количестве людей для каждого типа
+data = {
+    'ИЛЭ': 25,
+    'СЭИ': 30,
+    'ЭСЭ': 20,
+    'ЛИИ': 22,
+    'ЭИЭ': 18,
+    'ЛСИ': 24,
+    'СЛЭ': 16,
+    'ИЭИ': 28,
+    'СЭЭ': 15,
+    'ИЛИ': 27,
+    'ЛИЭ': 19,
+    'ЭСИ': 21,
+    'ЛСЭ': 17,
+    'ЭИИ': 23,
+    'ИЭЭ': 14,
+    'СЛИ': 26
+}
+
+# Создание столбчатой диаграммы
+types = list(data.keys())
+counts = list(data.values())
+
+plt.figure(figsize=(10, 6))
+plt.bar(types, counts, color='skyblue')
+plt.xlabel('Соционический Тип')
+plt.ylabel('Количество людей')
+plt.title('Статистика по типированиям по соционике сообщества My Typology')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Сохранение диаграммы как изображения
+image_path = 'sociotype_chart.png'
+plt.savefig(image_path)
+
+print("Диаграмма успешно создана и сохранена в файл sociotype_chart.png!")
